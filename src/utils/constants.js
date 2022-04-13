40ff0d411a4a843325b70b7efa99aa72099a24c8
samar-hassan <samarhassanphone@gmail.com>
2022-04-13 15:47:37 +0500

    fix: update url of leaderboard

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const BASE_ROUTE = '/';
 export const ROUTE_URLS = {
     homePage: `${BASE_ROUTE}`,
     login: `${BASE_ROUTE}login/`,
-    leaderBoard: `${BASE_ROUTE}leader_board/`,
+    leaderBoard: `${BASE_ROUTE}leaderboard/`,
     addQuestion: `${BASE_ROUTE}add/`,
     viewQuestion: `${BASE_ROUTE}questions/:id/`,
 }
